4ea2a056ad801f3f90f5071a92db7ed1b0db1af7
danieledt <danieledt@Xubuntu-16.04>
2018-06-27 13:17:39 +0200

    working

--- BD_JPA/src/entity/Satellite.java
+++ BD_JPA/src/entity/Satellite.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.*;
@@ -7,33 +9,62 @@ import javax.persistence.*;
 @Entity(name = "satellite")
 public class Satellite {
 	@Id
-	private int id;
-	@Column(name = "nome")
 	private String nome;
+	@Column(name = "agenzia")
+	private String agenzia;
+	@Column(name = "primaosservazione")
+	private Date primaOsservazione;
+	@Column(name = "termineattivita")
+	private Date termineAttivita;
+	
+
+	public String getAgenzia() {
+		return agenzia;
+	}
+
+
+	public void setAgenzia(String agenzia) {
+		this.agenzia = agenzia;
+	}
+
+
+	public Date getPrimaOsservazione() {
+		return primaOsservazione;
+	}
+
+
+	public void setPrimaOsservazione(Date primaOsservazione) {
+		this.primaOsservazione = primaOsservazione;
+	}
+
+
+	public Date getTermineAttivita() {
+		return termineAttivita;
+	}
+
+
+	public void setTermineAttivita(Date termineAttivita) {
+		this.termineAttivita = termineAttivita;
+	}
+
 
 	public static void main(String[] args) {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sample");
 		EntityManager em = emf.createEntityManager();
+		List<Satellite> sat = new ArrayList<Satellite>();
 
 		// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 		System.out.println("Test prendere tutti strumenti con satellite e mappe");
-		TypedQuery<Satellite> queryShowAll = em.createQuery("SELECT s FROM strumento s", Satellite.class);
+		TypedQuery<Satellite> queryShowAll = em.createQuery("SELECT s FROM satellite s", Satellite.class);
 		List<Satellite> resultAll = queryShowAll.getResultList();
 
 		for (Satellite s : resultAll) {
-			System.out.println(s.getId());
+			System.out.println(s.getNome() + " _ " + s.getAgenzia() + "  " + s.getPrimaOsservazione());
 		}
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getNome() {
 		return nome;
